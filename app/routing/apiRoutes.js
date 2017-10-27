5e8eb209fe2fb13b2720d6a6be82f762dd72c61e
ksp499 <ksp499@gmail.com>
2017-10-26 20:02:48 -0500

    accessing req.body's array not working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,40 +1,47 @@
-var friendData = require("../data/friends");
+var friendData = require("../data/friends.js");
+
+var path = require('path');
 
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
     res.json(friendData);
   });
 
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body-parser middleware
+
+  	  var input = req.body;
+
+  	  var scores = input.scores;
+
+  	  var name = '';
+  	  var photo = '';
+
+  	  var totalDifference = 10000;
+
+  	  for (var i = 0; i < 10; i++) {
+  	  	var difference = 0;
+  	  	for (var j = 0; j < 10; j++) {
+  	  		difference += Math.abs(friendData[i].scores[j] - scores[j]);
+  	  		console.log(difference);
+  	  	}
+
+  	  	if (difference < totalDifference) {
+  	  		totalDifference = difference;
+  	  		name = friendData[i].name;
+  	  		photo = friendData[i].photo;
+  	  	}
+  	  }
 
       friendData.push(req.body);
-      res.json(true);
+      res.json({
+      	name: name,
+      	photo: photo
+      });
 
   });
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
-  app.post("/api/clear", function() {
-    // Empty out the arrays of data
+  app.get("/api/clear", function() {
     friendData = [];
 
     console.log(friendData);
